89faa3f356f224e25a2cfa3fe11fb5989a79c70e
Francesco <x15010899@student.ncirl.ie>
2019-03-06 13:35:25 +0000

    mvc set up

---
+++ GymBuddy/Controllers/GymController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymBuddy.Controllers
+{
+    public class GymController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+
+    }
+}
--- GymBuddy/Startup.cs
+++ GymBuddy/Startup.cs
@@ -15,6 +15,7 @@ namespace GymBuddy
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,23 @@ namespace GymBuddy
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+            app.UseNodeModules(env);
+            
+            
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "Gym", Action = "Index" });
+            }
+            );
         }
     }
 }
